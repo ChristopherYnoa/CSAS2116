8cf05069cda270989f84153f50ae43106fa78aa1
Jasiel <58612443+Jgarcianinja@users.noreply.github.com>
2022-02-18 12:33:36 -0500

    insert updated

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ struct ssninfo
     string name;
 };
 
-int size = 1000;
+int arr_size = 1000;
+int array_length = 0;
 
 // exist : string -> boolean
 // purpose : To check whether the given ssn is exist or not
 bool exist(string ssn, struct ssninfo array[])
 {
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < arr_size; i++)
         if (array[i].ssn == ssn)
             return true;
 
@@ -28,8 +29,27 @@ bool exist(string ssn, struct ssninfo array[])
 // purpose : To add ssn and name in the array
 bool insert(string ssn, string name, struct ssninfo array[])
 {
+    bool a = exist(ssn, array);
+    if (a == true)
+    {
+        return false;
+    }
+    else
+    {
+        if (array_length >= arr_size)
+        {
+            arr_size = 2 * arr_size;
+            array[array_length].ssn = ssn;
+            array[array_length].name = name;
+        }
+        else
+        {
 
-    return true;
+            array[array_length].ssn = ssn;
+            array[array_length].name = name;
+        }
+        return true;
+    }
 }
 
 // del : string -> boolean
@@ -67,37 +87,26 @@ int main()
 
                 string ssn = tp.substr(2, 10);
                 string name = tp.substr(11);
-                cout << op << " : " << ssn << " " << name << "\n"; // print the data of the string
+                // cout << op << " : " << ssn << " " << name << "\n"; // print the data of the string
                 ssninfo *array = new ssninfo[1000];
-
-                // if (i < 15)
-                // {
-                //     array[i++] = ssn;
-                //     cout << "SSN:" << array[i - 1] << endl;
-                // }
 
                 if (op.compare("i") == 0)
                 {
-                    // array[i].ssn = ssn;
-                    bool a = exist(ssn, array);
-                    if (a == true)
+
+                    // cout << "SSN: " << array[i].ssn << "Name:" << array[i].name << endl;
+                    if (insert(ssn, name, array) == true)
                     {
-                        cout << "It exists" << endl;
-                    }
-                    else
-                    {
-                        cout << "Does not exist" << endl;
+                        iCounter++;
                     }
 
-                    cout << "SSN: " << array[i].ssn << endl;
-
-                    iCounter++;
+                    array_length++;
                 }
 
                 else if (op.compare("d") == 0)
                 {
 
                     dCounter++;
+                    array_length--;
                 }
 
                 else if (op.compare("r") == 0)
